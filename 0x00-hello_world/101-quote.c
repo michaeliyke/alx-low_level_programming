5ca0b701b5a9d2112cf071b9031721644e54a5d3
Michael Iyke <ymichaelc@gmail.com>
2023-06-18 14:49:58 +0100

    program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(str, sizeof(char), strlen(str), stdout);
 
+	fwrite(str, sizeof(char), strlen(str), stdout);
 	return (0);
 }
