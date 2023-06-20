bcdff736bb6cc79b5b78e163af064f7e9bf456bf
Michael Iyke <ymichaelc@gmail.com>
2023-06-20 14:17:52 +0000

    program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,11 +3,12 @@
 /**
  * main - prints stuff
  *
- * return: 0 for success and other number for failure
+ * Return: 0 for success and other number for failure
  */
 int main(void)
 {
 	char str[] = "_putchar\n";
+
 	write(1, str, strlen(str));
 	return (1);
 }
