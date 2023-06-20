6b032f70fc72fa0968044465fb308eada22bcc8f
Michael Iyke <ymichaelc@gmail.com>
2023-06-20 14:57:15 +0000

    cls

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+*print_alphabet - prints alphabets in lower case
+*
+*Return: does not return a value
+*/
+void print_alphabet(void)
+{
+	char chars[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(i, "0");
+	}
+	_putchar("\n", "");
+
+	return (0);
+}
+
+
+/**
+ * main - prints stuff
+ *
+ * Return: 0 for success and other number for failure
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+
+	write(1, str, strlen(str));
+	return (0);
+}
