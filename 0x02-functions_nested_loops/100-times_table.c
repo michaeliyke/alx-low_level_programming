e649c3d69802bc040133946d77ccdce889aadd23
Michael Iyke <ymichaelc@gmail.com>
2023-06-22 15:18:41 +0100

    function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,8 +9,7 @@
  */
 void print_times_table(int n)
 {
-	int a, b, c, d3, d2, d1;
-	double d;
+	int a, b, c;
 
 	if (n > 15 || n < 0)
 		return;
@@ -20,39 +19,20 @@ void print_times_table(int n)
 		for (b = 0; b < n + 1; b++)
 		{
 			c = a * b;
-
 			if (b == 0)
-			{
-				/* _putchar(b + '0'); */
 				_putchar('0');
-			}
 			if (b > 0 && c < 10)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				comma_n_space(3);
 				_putchar(c + '0');
 			}
 			else if (c > 9 && c < 100)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
+				print_2_digits(c);
 			}
 			else if (c > 99)
 			{
-				d3 = c % 10; /* 3rd digit */
-				d = c / 10;
-				d2 = (int)d % 10; /* 2nd digit */
-				d = d / 10;
-				d1 = (int)d % 10; /* 1st digit */
-
-				_putchar(' ');
-				_putchar(',');
-				_putchar(d1 + '0');
-				_putchar(d2 + '0');
-				_putchar(d3 + '0');
+				print_3_digits(c);
 			}
 		}
 		_putchar('\n');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,4 +13,7 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+void comma_n_space(int);
+void print_3_digits(int);
+void print_2_digits(int);
 #endif
