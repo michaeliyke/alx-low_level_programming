5d876830eb1c8c8b0a96d11b78ac1e5f8497991a
Michael Iyke <ymichaelc@gmail.com>
2023-06-22 08:36:48 +0100

    function that prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include "stdio.h"
 
-void print_ints(int);
-
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  *
@@ -12,41 +10,30 @@ void jack_bauer(void)
 {
 	int i;
 	int j;
-	for (i = 0; i < 24; i++)
-	{
-		for (j = 0; j < 60; j++)
-		{
-			print_ints(i);
-			/* putchar(':' + '0');*/
-			/* print_ints(j);*/
-		}
-		putchar('\n');
-	}
-}
-
-/**
- * print_ints - prints the digits of numbers 0-99
- *
- * Return: void
- */
-void print_ints(int i)
-{
 	int last;
 	int first;
 	double flt;
 
-	if (i < 10)
+	for (i = 0; i < 24; i++)
 	{
-		putchar('0');
-		putchar(i + '0');
-	}
-	else
-	{
-		last = i % 10;
-		flt = i / 10;
-		first = (int)flt;
+		for (j = 0; j < 60; j++)
+		{
+			last = i % 10;
+			flt = i / 10;
+			first = (int)flt;
 
-		putchar(first + '0');
-		putchar(last + '0');
+			_putchar('0' + first);
+			_putchar('0' + last);
+
+			last = j % 10;
+			flt = j / 10;
+			first = (int)flt;
+
+			_putchar(':');
+			_putchar('0' + first);
+			_putchar('0' + last);
+
+			_putchar('\n');
+		}
 	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
@@ -9,3 +11,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_ints(int);
+#endif
