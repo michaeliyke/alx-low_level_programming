6f849c4706f03d76f82e838b474595be1e7bf27c
Michael Iyke <ymichaelc@gmail.com>
2023-06-27 01:16:44 +0100

    function that prints n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
+int len_str(char *s);
+
 /**
  * print_array - prints n elements of an array of integers
  * @a: the array whose elemets re printed
@@ -11,10 +13,31 @@
 void print_array(int *a, int n)
 {
 	int i;
+	int l = len_str(a);
+
+	if (l != n)
+		n = l;
 
 	for (i = 0; i < n - 1; i++)
 	{
 		printf("%d, ", a[i]);
 	}
 	printf("%d\n", a[n - 1]);
+}
+
+/**
+ * len_str - compute the length of a string
+ * @s: the str for which length is computed
+ *
+ * Return: the length of the string
+ */
+int len_str(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i - 1);
 }
