3fef481bc5a411ed5125f1a327fc8443b80bfbdb
Michael Iyke <ymichaelc@gmail.com>
2023-06-27 11:11:42 +0100

    function that copies the string pointed to by src, including the terminating null

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,4 +8,36 @@
  */
 char *_strcpy(char *dest, char *src)
 {
+	int i = len_str(src);
+	char *ptr = dest;
+
+	while (i > 0 && *src != '\0')
+	{
+		*dest++ = *src++;
+		i--;
+	}
+	while (i > 0)
+	{
+		*dest++ = '\0';
+		i--;
+	}
+	return ptr;
+}
+
+/**
+ * len_str - compute the length of a string.
+ * It counts the string without the trailing null character
+ * @s: the str for which length is computed
+ *
+ * Return: the length of the string
+ */
+int len_str(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
