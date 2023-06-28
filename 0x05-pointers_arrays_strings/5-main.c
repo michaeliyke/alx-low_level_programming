1a8ceb8e09925cfeec927f7083729d3e42b25678
Michael Iyke <ymichaelc@gmail.com>
2023-06-29 00:52:44 +0100

    function that reverses a string

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -1,5 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+void reverse(char *src);
+char *strcopy(char *string);
+int strlength(char *string);
+void strreverse_2(char *string);
+void strreverse_3(char *s);
+void strinvert(char *s);
 
 /**
  * main - check the code
@@ -8,10 +16,130 @@
  */
 int main(void)
 {
-	char s[10] = "My School";
+	/* char s[10] = "My School"; */
 
-	printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
+	/* printf("%s\n", s); */
+	/* rev_string(s); */
+	/* reverse(s); */
+	/* printf("%s\n", s); */
+
+	/* int a[10];
+	int i = 10; */
+
+	/* Change value of first position */
+	/* *a = 31;
+	*(a + 1) = 198;
+	*(a + 2) = 298;
+	*(a + 3) = 398;
+	*(a + 4) = 498;
+	*(a + 5) = 598;
+	*(a + 6) = 698;
+	*(a + 7) = 798;
+	*(a + 8) = 898;
+	*(a + 9) = 998;
+
+	while (i-- > 0)
+	{
+		printf("Address: %p, Value: %d\n", (a + i), *(a + i));
+	} */
+
+	char x[] = "School of my choice";
+	char *s = x;
+	char y[7];
+
+	strinvert(s);
+	printf("\nREVERSED is: %s\n", x);
+
 	return (0);
+}
+
+void strinvert(char *a)
+{
+	char *b = a;
+	char temp;
+
+	while (*a != '\0')
+		a++;
+	a--;
+
+	while (a > b)
+	{
+		temp = *a;
+		*a = *b;
+		*b = temp;
+		a--;
+		b++;
+	}
+}
+
+void strreverse_2(char *str)
+{
+	char *t1 = str;
+	char *t2 = str;
+
+	while (*t1 != '\0')
+	{
+		t1++;
+	}
+
+	t1--;
+
+	while (t1 > t2)
+	{
+		char t3 = *t1;
+		*t1 = *t2;
+		*t2 = t3;
+		t2++;
+		t1--;
+	}
+}
+
+void strreverse_3(char *s)
+{
+	char *i = s;
+	char *j = s;
+
+	while (*j != '\0')
+	{
+		j++;
+	}
+	j--;
+	while (i < j)
+	{
+		char k = *i;
+		*i = *j;
+		*j = k;
+		i++;
+		j--;
+	}
+}
+
+/**
+ * strleng - Returns the length of a string
+ * *string++ moves the address pointed to by string adress from
+ * first element to the next and next till the end of the string
+ * @string: the address of the input string
+ *
+ * Return: int length of the string
+ */
+int strlength(char *str)
+{
+	int len = 0;
+	while (*str++ != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+char *strcopy(char *str)
+{
+	char *y = "School";
+
+	while (*y++ != '\0')
+	{
+		*str++ = *y;
+	}
+	*str++ = '\0';
+	return (str);
 }
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-char *cpy(char x[], char *s);
+char *cpy(char *dest, char *src);
 
 /**
  * rev_string -  reverses a string.
@@ -10,7 +10,75 @@ char *cpy(char x[], char *s);
  *
  * Return: void
  */
-void rev_string(char *s)
+void rev_string(char *a)
 {
-	_putchar(s);
+	char *b = a;
+	char temp;
+
+	while (*a != '\0')
+		a++;
+	a--;
+
+	while (a > b)
+	{
+		temp = *a;
+		*a = *b;
+		*b = temp;
+		a--;
+		b++;
+	}
+}
+
+/**
+ * _strcpy - copies *src, including the \0 char,to dest
+ * @dest: the destination of copy operation
+ * @src: the source of copy operation
+ *
+ * Return: pointer to str
+ */
+char *cpy(char *dest, char *src)
+{
+	int i = len_str(src);
+	char *ptr = dest;
+
+	while (i > 0 && *src != '\0')
+	{
+		*dest++ = *src++;
+		i--;
+	}
+	*dest++ = '\0';
+	return (ptr);
+}
+
+char *strncpy(char *dest, const char *src, size_t n)
+{
+	char *ptr = dest;
+	while (n > 0 && *src != '\0')
+	{
+		*dest++ = *src++;
+		n--;
+	}
+	while (n > 0)
+	{
+		*dest++ = '\0';
+		n--;
+	}
+	return ptr;
+}
+
+/**
+ * len_str - compute the length of a string
+ * @s: the str for which length is computed
+ *
+ * Return: the length of the string
+ */
+int len_str(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
