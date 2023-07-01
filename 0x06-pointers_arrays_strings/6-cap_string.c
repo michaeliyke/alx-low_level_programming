e58ec0b72c2e56b41c7323f064f167d1506e2d62
Michael Iyke <ymichaelc@gmail.com>
2023-07-01 21:57:40 +0100

    function that capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+int is_sep(char c);
+
+/**
+ * cap_string - function that capitalizes all words of string
+ * @s: input string
+ *
+ * Return: Pointer to string
+ */
+char *cap_string(char *s)
+{
+	char *t = s;
+	int count = 0;
+
+	// No need to attempt to convert first one
+	while (*t != '\0')
+	{
+		if (is_sep(*t))
+		{
+			count = 0;
+		}
+		else if (count == 0)
+		{
+			if ((*t >= 97 && *t <= 122))
+			{
+				*t = *t - 32;
+				count = 1;
+			}
+			else
+			{
+				count = 1;
+			}
+		}
+		t++;
+	}
+	return (s);
+}
+
+/**
+ * is_sep - Determine if a char is a separators
+ * @c: input char
+ *
+ * Return: 1 for true and 0 for false
+ */
+int is_sep(char c)
+{
+	int separators[] = {9, 10, 13, 32, 34, 40, 41, 44, 46, 58, 59, 63, 123, 125};
+	int length = sizeof(separators) / sizeof(int);
+
+	while (--length >= 0)
+	{
+		if (separators[length] == c)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
