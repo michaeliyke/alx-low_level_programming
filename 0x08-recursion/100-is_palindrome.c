18b228e16a35e32ec90286d5c314e3aaa22f8588
Michael Iyke <ymichaelc@gmail.com>
2023-07-07 00:48:43 +0100

    function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,9 +9,9 @@
 int is_palindrome(char *s)
 {
 	char *start = s;
-	char *end = s + strleng(s, 0) - 1;
+	char *end = s + strlen_(s, 0) - 1;
 
-	return (is_palindrome_(s, start, end));
+	return (is_palindrome_(start, end));
 }
 
 /**
@@ -22,22 +22,45 @@ int is_palindrome(char *s)
  *
  * Return: 1 if s is a palindrome and 0 if not
  */
-int is_palindrome_(char *s, char *start, char *end)
+int is_palindrome_(char *start, char *end)
 {
-	lower(start), lower(end);
-	if (*start != *end)
+	if (start >= end)
+		return (1);
+	if (tolower_(*start) != tolower_(*end))
 	{
-		if (skip(*start))
-			return (is_palindrome_(s, start + 1, end));
-		if (skip(*end))
-			return (is_palindrome_(s, start, end - 1));
+		if (!(isalpha_(*start)))
+			return (is_palindrome_(start + 1, end));
+		if (!(isalpha_(*end)))
+			return (is_palindrome_(start, end - 1));
 		return (0);
 	}
 
-	if (((start + 1) == (end - 1)) || ((start + 1) > (end - 1)))
-		return (1);
+	return (is_palindrome_(start + 1, end - 1));
+}
 
-	return (is_palindrome_(s, start + 1, end - 1));
+/**
+ * lower - convert a char to lowercase
+ * @c: char to be converted
+ *
+ * Return: void
+ */
+char tolower_(char c)
+{
+	if (c > 64 && c < 91)
+		return (c + 32);
+	else
+		return (c);
+}
+
+/**
+ * skip - skips a char if it is not an alphabet
+ * @c: char to skip
+ *
+ * Return: 1 if char should be skipped, 0 if not
+ */
+int isalpha_(char c)
+{
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
 
 /**
@@ -47,33 +70,7 @@ int is_palindrome_(char *s, char *start, char *end)
  *
  * Return: length og the string
  */
-int strleng(char *s, int i)
+int strlen_(char *s, int i)
 {
-	return ((*s != '\0') ? (strleng(++s, ++i)) : (i));
-}
-
-/**
- * skip - skips a char if it is not an alphabet
- * @c: char to skip
- *
- * Return: 1 if char should be skipped, 0 if not
- */
-int skip(char c)
-{
-	if ((c > 64 && c < 91) || (c > 96 && c < 123))
-		return (0);
-	else
-		return (1);
-}
-
-/**
- * lower - convert a char to lowercase
- * @c: char to be converted
- *
- * Return: void
- */
-void lower(char *c)
-{
-	if (*c > 64 && *c < 91)
-		*c += 32;
+	return ((*s != '\0') ? (strlen_(++s, ++i)) : (i));
 }
