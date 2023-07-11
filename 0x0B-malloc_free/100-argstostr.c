727f56c3109b5f8dedcf52d2da0f8755b877d9ae
Michael Iyke <ymichaelc@gmail.com>
2023-07-11 13:38:11 +0100

    function that concatenates all the arguments of your program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * argstostr - concats all its args into a single str sep by newline
+ * @argc: arguments count
+ * @argv: array of strings containing all the given arguments
+ *
+ * Return: pointer to the new concatened string
+ */
+char *argstostr(int argc, char **argv)
+{
+	char *word, *final_str, *ptr_final;
+	int i, size = 0;
+
+	(void)final_str;
+	(void)argc;
+	(void)argv;
+
+	for (i = 0; i < argc; i++)
+	{
+		word = argv[i];
+		size += (count(word) + 1); /* + 1 because of the newlines */
+	}
+
+	final_str = malloc(sizeof(*final_str) * size);
+	ptr_final = final_str;
+
+	for (i = 0; i < argc; i++)
+	{
+		word = argv[i];
+		while (*word != '\0')
+		{
+			*ptr_final = *word;
+			++word, ++ptr_final;
+		}
+		*ptr_final = '\n';
+		++ptr_final;
+	}
+	return (final_str);
+}
+
+/**
+ * count - get the char count of a string starting.
+ * Count starts from 0 and includes null terminator
+ * @str: pointer the string
+ *
+ * Return: interger length of the string
+ */
+int count(char *str)
+{
+	char *p = str;
+
+	if (str == NULL)
+		return (0);
+	while (*p != '\0')
+		++p;
+	return (p - str);
+}
