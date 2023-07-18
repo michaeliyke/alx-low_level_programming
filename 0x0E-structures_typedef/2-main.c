a8385cea5aa54bdea03329ec9853618a171a3c4a
Michael Iyke <ymichaelc@gmail.com>
2023-07-18 14:58:58 +0000

    corection

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -1,8 +1,9 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
 
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
@@ -10,9 +11,9 @@ int main(void)
 {
 	struct dog my_dog;
 
-	my_dog.name = "Poppy";
-	my_dog.age = 3.5;
-	my_dog.owner = "Bob";
+	my_dog.name = NULL;
+	my_dog.age = .0f;
+	my_dog.owner = NULL;
 	print_dog(&my_dog);
 	return (0);
 }
