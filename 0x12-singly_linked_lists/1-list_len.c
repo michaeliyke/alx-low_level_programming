6c69305eca2b16b25e9e5e378f7935c15a2d6ab2
Michael Iyke <ymichaelc@gmail.com>
2023-07-28 08:59:28 +0000

    corrections

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,17 @@
 #include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n;
+
+	if (h == NULL)
+		return (0);
+	for (n = 0; h != NULL; n++)
+	{
+		h = h->next;
+	}
+	return (n);
+}
