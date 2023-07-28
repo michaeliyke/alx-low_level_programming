e58bc317e11dced6148c214be0e6f645566531a3
Michael Iyke <ymichaelc@gmail.com>
2023-07-28 10:29:00 +0000

    corrections

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -36,31 +36,3 @@ int _strlen(char *string)
 		++string;
 	return ((string - pointer));
 }
-
-/**
- * print_list - prints all the elements of a list_t list
- * @h: the head of the linked list
- *
- * Return: the number of elements of the linked list
- */
-size_t print_list(const list_t *h)
-{
-	size_t n;
-
-	if (h == NULL)
-		return (0);
-	for (n = 0; h != NULL; n++)
-	{
-		if (h->str == NULL)
-		{
-			printf("[%d] %s\n", 0, "(nil)");
-		}
-		else
-		{
-
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		h = h->next;
-	}
-	return (n);
-}
