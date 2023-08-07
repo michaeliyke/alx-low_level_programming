4f768fcadf55ddb46e221d1a66d4bc81a63c5e0f
Michael Iyke <ymichaelc@gmail.com>
2023-08-07 15:39:31 +0000

    reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: a path to read file contents from
+ * @letters: letters is the number of letters it should read and print
+ *
+ * Return:  actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *str = malloc(sizeof(*str) * letters);
+	ssize_t num_read, num_written;
+	int fd;
+
+	if (filename == NULL || str == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY); /* open */
+	if (fd == -1)
+		return (0);
+
+	num_read = read(fd, str, letters); /* read */
+	if (num_read == -1)
+		return (0);
+
+	num_written = write(STDOUT_FILENO, str, num_read); /* write */
+	if (num_written < num_read)
+		return (0);
+	close(fd);
+	free(str);
+	return (num_written);
+}
+
+/**
+ * _strlen - returns length of a string
+ * @string: input string whose length is needed
+ *
+ * Return: length of string
+ */
+size_t _strlen(char *string)
+{
+	char *pointer = string;
+
+	if (string == NULL || *string == '\0')
+		return (0);
+	while (*string != '\0')
+		++string;
+	return ((string - pointer));
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+size_t _strlen(char *string);
+
+#endif
