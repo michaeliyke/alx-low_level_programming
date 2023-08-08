78591b008b4183401c324ccd7cb034497302c76d
Michael Iyke <ymichaelc@gmail.com>
2023-08-08 12:15:58 +0000

    function that appends text at the end of a file

--- 0x15-file_io/main.c
+++ 0x15-file_io/main.c
@@ -1,10 +1,9 @@
-
 #include "main.h"
 
 /**
  * main - check the code
- * @ac: arg count
- * @av: argv list
+ * @ac: ac
+ * @av: av
  *
  * Return: Always 0.
  */
@@ -14,15 +13,12 @@ int main(int ac, char **av)
 
 	(void)ac;
 	(void)av;
-
-	/**
-	 * if (ac != 3)
-	 * {
-	 * dprintf(2, "Usage: %s filename text\n", av[0]);
-	 * exit(1);
-	 * }
-	 */
-	res = create_file("abc", NULL);
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
+	res = append_text_to_file(av[1], av[2]);
 	printf("-> %i)\n", res);
 	return (0);
 }
