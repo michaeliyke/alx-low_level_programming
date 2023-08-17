72dda644e3e0266ed159eca0043868959a0bfc7f
Michael Iyke <ymichaelc@gmail.com>
2023-08-17 09:23:46 +0000

    updates

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define BUFFER_SIZE 1024
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -10,4 +11,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 size_t _strlen(char *string);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void f_copy(int *fd_from, int *fd_to, char *from_pth, char *to_pth);
+
 #endif
