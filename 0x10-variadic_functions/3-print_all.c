81ad00a36a60afb1c5362e918ddd54387820d0db
Michael Iyke <ymichaelc@gmail.com>
2023-08-19 14:40:42 +0000

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,26 +8,79 @@
  *
  * Return: void
  */
-void print_all(const char *const format, ...)
+void print_all(const char *const fmt, ...)
 {
+	va_list ap;
+	int i = 0;
+	char c, *sep = ", ";
+
+	if (fmt == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, fmt);
+	c = fmt[0];
+	while (c != '\0')
+	{
+		if (fmt[i + 1] == '\0')
+			sep = "";
+		switch (c)
+		{
+		case '\0': /* % at pos \0 means no formatter */
+			return;
+		case 'c':
+			printf("%c%s", va_arg(ap, int), sep);
+			break;
+		default:
+			/* print other formats here*/
+			print_all_ext(c, ap, sep);
+		}
+		i++;
+		c = fmt[i];
+	}
+	printf("\n");
 }
 
-void intprint(int *x)
+/**
+ * print_all_ext - print other specs
+ * @c: the character spec
+ * @ap: the va_list object where arguments were obtained from
+ * @sep: output seprator
+ *
+ * Return: void
+ */
+void print_all_ext(char c, va_list ap, char *sep)
 {
-	printf("%d", *x);
+	char *s;
+
+	switch (c)
+	{
+	case 's': /* handle string printing */
+		s = va_arg(ap, char *);
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s%s", s, sep);
+		break;
+	case 'f': /* handle floats printing */
+		printf("%f%s", va_arg(ap, double), sep);
+		break;
+
+	case 'i': /* handle integer printing */
+		printf("%i%s", va_arg(ap, int), sep);
+		break;
+	}
 }
 
-void charprint(char *c)
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar_(char c)
 {
-	printf("%c", *c);
-}
-
-void floatprint(float *x)
-{
-	printf("%f", *x);
-}
-
-void strprint(int *x)
-{
-	printf("%s", x);
+	fflush(stdout);
+	return (write(1, &c, 1));
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,16 +3,13 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int _putchar(char c);
+int _putchar_(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);
-void getfmt(char *c);
-void intprint(int *x);
-void charprint(char *c);
-void floatprint(float *x);
-void strprint(int *x);
+void print_all_ext(char c, va_list ap, char *sep);
 
 #endif
