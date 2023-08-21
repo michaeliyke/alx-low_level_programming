37c82a2f94f8ef3db0e129dadcd7d707116ab1b6
Michael Iyke <ymichaelc@gmail.com>
2023-08-21 15:33:37 +0000

    update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -29,3 +29,74 @@ size_t free_listint_safe(listint_t **head)
 	}
 	return (i);
 }
+
+/**
+ * print_int - Print an integer to the std output
+ * @n: interger to print
+ *
+ * Return: void
+ */
+int print_int(long int n)
+{
+	char buff[10], temp;
+	int i = 0, sign = 1, j;
+
+	if (n < 0)
+	{
+		sign = -1;
+		n = -n;
+	}
+	do {
+		buff[i++] = '0' + n % 10;
+		n /= 10;
+	} while (n > 0);
+	if (sign < 0)
+		buff[i++] = '-';
+	buff[i] = '\0';
+	j = 0;
+	i--;
+	while (i > j)
+	{
+		temp = buff[j];
+		buff[j] = buff[i];
+		buff[i] = temp;
+		i--;
+		j++;
+	}
+	return (_writestring(buff));
+}
+
+/**
+ * _writestring - write a string to the stdout followed by newline
+ * @str: string to write
+ *
+ * Return: number of bytes written or -1 on error
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _writestring(char *str)
+{
+
+	ssize_t bytes_written;
+
+	if (str == NULL)
+		return (-1);
+	bytes_written = write(STDOUT_FILENO, str, _strlen(str));
+	return (bytes_written > 0 ? bytes_written : -1);
+}
+
+/**
+ * _strlen - returns length of a string
+ * @string: input string whose length is needed
+ *
+ * Return: length of string
+ */
+int _strlen(char *string)
+{
+	char *pointer = string;
+
+	if (string == NULL || *string == '\0')
+		return (0);
+	while (*string != '\0')
+		++string;
+	return ((string - pointer));
+}
--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,13 +2,15 @@
 
 /**
  * find_listint_loop -  finds the loop in a linked list
- * @h: pointer of head
+ * @head: pointer of head
  *
  * Return: address of the statrt of the loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *joint, *end = end_of_list(head, &joint);
+
+	(void)end;
 
 	return (joint);
 }
@@ -54,76 +56,4 @@ listint_t *end_of_list(listint_t *head, listint_t **joint)
 		*joint = slow->next; /* slow however equals the last element */
 	}
 	return (slow);
-}
-
-/**
- * print_int - Print an integer to the std output
- * @n: interger to print
- *
- * Return: void
- */
-int print_int(long int n)
-{
-	char buff[10], temp;
-	int i = 0, sign = 1, j;
-
-	if (n < 0)
-	{
-		sign = -1;
-		n = -n;
-	}
-	do
-	{
-		buff[i++] = '0' + n % 10;
-		n /= 10;
-	} while (n > 0);
-	if (sign < 0)
-		buff[i++] = '-';
-	buff[i] = '\0';
-	j = 0;
-	i--;
-	while (i > j)
-	{
-		temp = buff[j];
-		buff[j] = buff[i];
-		buff[i] = temp;
-		i--;
-		j++;
-	}
-	return (_writestring(buff));
-}
-
-/**
- * _writestring - write a string to the stdout followed by newline
- * @str: string to write
- *
- * Return: number of bytes written or -1 on error
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _writestring(char *str)
-{
-
-	ssize_t bytes_written;
-
-	if (str == NULL)
-		return (-1);
-	bytes_written = write(STDOUT_FILENO, str, _strlen(str));
-	return (bytes_written > 0 ? bytes_written : -1);
-}
-
-/**
- * _strlen - returns length of a string
- * @string: input string whose length is needed
- *
- * Return: length of string
- */
-int _strlen(char *string)
-{
-	char *pointer = string;
-
-	if (string == NULL || *string == '\0')
-		return (0);
-	while (*string != '\0')
-		++string;
-	return ((string - pointer));
 }
