5c9bb00ac5f64be9d52a54f74570c3a53576b873
Michael Iyke <ymichaelc@gmail.com>
2023-09-28 19:36:14 +0100

    function that sets the value of a bit to 0 at a given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the nth bit of an integer to 1
+ * @n: pointer to unsigned long interger input
+ * @index: index is the index, starting from 0 of the bit you want to set
+ *
+ * Return: 1 if success and -1 if fails
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	/**
+	 * long int is 4 bytes in 32 bits and 8 bytes in 64
+	 * Since a byte is 8 bits
+	 * the maths below: 4*8 = 32, 8*8 = 64
+	 * This helps us know which arch
+	 */
+	unsigned int bit_arch = (sizeof(unsigned long int) * 8);
+
+	if (index > bit_arch)
+		return (-1);
+	*n &= (1 << index);
+
+	return (1);
+}
