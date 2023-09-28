696ad6486d382acc3757637402bb5d43bcb6864a
Michael Iyke <ymichaelc@gmail.com>
2023-09-28 18:58:04 +0100

    function that returns the value of a bit at a given index.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,10 +14,23 @@
  * compare & 0001000 - 1000 -- mask = 1<<(4-1) = 1<<(index-1)
  * result =  0000000  - 0 --- result
  * Therefore: 4th bit is 0
+ *
+ * Edge cases are
+ * --------------
+ * 1. index should not be > 32 on 32 bit machine and 64 on 64
+ * 2. index cannot be negative
+ * This is the only real edge cases. Others such as
+ * i. index cannot be >= n
+ * ii. (1<<index) cannot be > n
+ * These aren't really a problem because the bits beyond n will
+ * simply be zero and so these result at such points will be 0
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int mask;
+	unsigned int mask;
+
+	if (index > 64)
+		return (0);
 
 	mask = (1 << (index));
 	return ((n & mask) == 0 ? 0 : 1);
