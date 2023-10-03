85f7b53e04f8733aa02b0945c21ba81bdc616a44
Michael Iyke <ymichaelc@gmail.com>
2023-10-03 03:08:05 +0100

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,4 +14,9 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void f_copy(int *fd_from, int *fd_to, char *from_pth, char *to_pth);
 void closefd(int fd);
+
+#define W_USAGE "Usage: cp file_from file_to\n"
+#define E_RD "Error: Can't read from file %s\n"
+#define E_WR "Error: Can't write to %s\n"
+#define E_CLOSE "Error: Can't close fd %d\n"
 #endif
