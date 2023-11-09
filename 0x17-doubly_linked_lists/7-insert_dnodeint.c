b26ca00740e455a3d0279d2d114f57e1478eb2db
Michael Iyke <ymichaelc@gmail.com>
2023-11-09 15:51:00 +0000

    correct

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,35 +15,39 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int index, int n)
 	dlistint_t *tmp, *node;
 	size_t i;
 
-	if (h == NULL)
-		return (NULL);
-
 	node = malloc(sizeof(dlistint_t));
-	if (node == NULL)
+	if (h == NULL || node == NULL)
 		return (NULL);
 
 	node->n = n;
 	node->prev = NULL;
 	node->next = NULL;
 
-	tmp = *h;
-	for (i = 0; tmp && tmp->next; tmp = tmp->next, i++)
-		if (i + 1 == index)
-			break;
+	/* pos 0 insert - no elements prior */
+	if (*h == NULL && index == 0)
+	{
+		*h = node;
+		return (node);
+	}
+	else if (*h != NULL && index == 0)
+	{
+		node->next = *h;
+		node->prev = (*h)->prev;
+		(*h)->prev = node;
+		return (node);
+	}
+	else if (*h == NULL)
+		return (NULL);
 
-	if (i + 1 == index) /* insert here */
-	{
-		node->next = tmp;
-		node->prev = tmp->prev;
-		tmp->prev->next = node;
-		tmp->prev = node;
-		return (node);
-	}
-	if (i + 1 == index && tmp->next == NULL)
-	{
-		tmp->next = node;
-		node->prev = tmp;
-		return (node);
-	}
+	/* Below: *h is not NULL and index != 0 */
+	for (tmp = *h, i = 0; tmp && tmp->next; tmp = tmp->next, i++)
+		if (i == index)
+		{
+			node->next = tmp;
+			node->prev = tmp->prev;
+			tmp->prev->next = node;
+			tmp->prev = node;
+			return (node);
+		}
 	return (NULL);
 }
