1d64ca53cf424d4e35c2729caab97144995368d5
Michael Iyke <ymichaelc@gmail.com>
2023-11-17 08:41:38 +0000

    task 100

---
+++ 0x18-dynamic_libraries/100-tests.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdalign.h>
+
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: integer sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: integer result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * div - divide two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: integer result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: integer result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * mod - modulus of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: integer result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
