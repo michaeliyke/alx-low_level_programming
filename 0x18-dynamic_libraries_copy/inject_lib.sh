83cca3adfb3b51b64791b05cfcbae6b2a6e64d60
Michael Iyke <ymichaelc@gmail.com>
2023-11-17 21:57:50 +0000

    correction

--- 0x18-dynamic_libraries_copy/inject_lib.sh
+++ 0x18-dynamic_libraries_copy/inject_lib.sh
@@ -4,7 +4,7 @@ script=$1 # full path of calling script
 if [[ ! -f ../libhack.so ]]; then
 	wget https://raw.githubusercontent.com/michaeliyke/alx-low_level_programming/master/0x18-dynamic_libraries_copy/libhack.so -P ..
 fi
-fp=$(echo realpath "../libhack.so")
+fp=$(echo realpath "./libhack.so")
 echo "export LD_PRELOAD=$fp" >> ~/.bashrc
 source ~/.bashrc
 echo $LD_PRELOAD
