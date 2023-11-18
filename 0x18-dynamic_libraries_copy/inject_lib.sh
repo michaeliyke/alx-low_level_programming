00263a217b65e115bcea00526054da3732533800
Michael Iyke <ymichaelc@gmail.com>
2023-11-18 20:49:29 +0000

    correction

--- 0x18-dynamic_libraries_copy/inject_lib.sh
+++ 0x18-dynamic_libraries_copy/inject_lib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 script=$1 # full path of calling script
-lib=$(realpath ../../libhack.so)
+lib=$(realpath ../libhack.so)
 if [[ ! -f $lib ]]; then
 	wget https://raw.githubusercontent.com/michaeliyke/alx-low_level_programming/master/0x18-dynamic_libraries_copy/libhack.so -P ..
 fi
