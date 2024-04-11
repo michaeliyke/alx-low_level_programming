48bc14bfd196c6cd4b0fa64485f6a3e338f399d7
Michael Iyke <ymichaelc@gmail.com>
2024-04-11 13:17:48 +0100

    104: advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,5 +1,22 @@
 #include "search_algos.h"
 /**
+ * print_array - prints array
+ * @array: array
+ * @size: size
+ *
+ * Return: void
+ */
+void print_array(int *array, size_t size)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = 0; i < (int)size - 1; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+}
+
+/**
  * advanced_binary - searches for a value in a sorted array of integers using
  * the Binary search algorithm
  * @array: pointer to the first element of the array to search in
@@ -10,27 +27,25 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	int result, mid, i;
+	size_t left = 0, right = size - 1, mid, i;
 
-	if (array == NULL || size <= 0)
+	if (array == NULL)
 		return (-1);
-	printf("Searching in array: ");
-	for (i = 0; i < (int)size - 1; i++)
-		printf("%d, ", array[i]);
-	printf("%d\n", array[i]);
-
-	mid = size >> 1;
-	if (value == array[mid])
+	while (left <= right)
 	{
-		if (mid == 0 || array[mid - 1] != value)
-			return (mid);
-	}
+		mid = (left + right) >> 1;
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
 
-	if (value > array[mid]) /* Adjust to handle sub-array */
-	{
-		mid++;
-		result = advanced_binary(array + mid, size - mid, value);
-		return (result == -1 ? -1 : mid + result);
+		if (array[mid] == value)
+			if (mid == 0 || array[mid - 1] != value)
+				return (mid);
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
 	}
-	return (advanced_binary(array, mid, value));
+	return (-1);
 }
